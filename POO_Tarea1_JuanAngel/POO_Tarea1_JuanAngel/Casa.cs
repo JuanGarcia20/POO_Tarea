badf56cfb1fd620f7c6738916be35a63ae059bcd
JuanGarcia20 <115969644+JuanGarcia20@users.noreply.github.com>
2023-03-02 21:12:59 -0600

    Add files via upload

---
+++ POO_Tarea1_JuanAngel/POO_Tarea1_JuanAngel/Casa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Tarea1_JuanAngel
+{
+    internal class Casa
+    {
+        public string nombreDuenoCasa, apellidoDuenoCasa, maerialCasa;
+        double precioVentaCasa, engancheCasa;
+
+       public ConsoleColor colorCasa;
+
+        //propiedades
+        public double EngancheCasa
+        {
+            get { return engancheCasa; } //obtener
+            set { engancheCasa = value; }//enviar
+        }
+
+        public ConsoleColor ColorCasa
+        {
+            get { return colorCasa; } //obtener
+            set { colorCasa = value; }//enviar
+
+        }
+
+        public double PrecioVentaCasa
+        {
+            get { return precioVentaCasa; } //obtener
+            set { precioVentaCasa = value; }//enviar
+
+        }
+
+    }
+}
---
+++ POO_Tarea1_JuanAngel/POO_Tarea1_JuanAngel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Tarea1_JuanAngel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear una instancia
+            //NombreClase nombreObjeto = new NombreClase
+            Casa objetoCasa1 = new Casa();
+            Casa objetoCasa2 = new Casa();
+            Casa objetoCasa3 = new Casa();
+            Casa objetoCasa4 = new Casa();
+
+            //usar objeto
+            objetoCasa1.nombreDuenoCasa = "Juan Garcia";
+
+
+        }
+    }
+}
